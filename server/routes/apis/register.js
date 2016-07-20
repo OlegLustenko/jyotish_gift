1f6488c78e7148cc8fb1d27765520ec652c46429
Oleg <oleg.lustenko@gmail.com>
2016-07-21 01:57:31 +0300

    minor

--- server/routes/apis/register.js
+++ server/routes/apis/register.js
@@ -8,20 +8,23 @@ export default(router) => {
   router
     .post('/user/auth', async function (ctx, next) {
       // let user;
-      let {  ...req } = ctx.request.body;
-      console.log(ctx.user);
-      let user = await User.find(req).exec();
-      console.log(user);
+      let { ...req } = ctx.request.body;
+
+      let user = await User
+        .find(req)
+        .exec();
+
       if (!user.length) {
-        user = await User
-          .create({firstName: req.firstName, lastName: req.lastName, email: req.email, password: req.password})          
+        user = await User.create({firstName: req.firstName, lastName: req.lastName, email: req.email, password: req.password,})
       }
       ctx.body = user;
       await next();
     })
     .get('/users', async(ctx, next) => {
       console.log('qwe');
-      let users = await User.find({}).lean();
+      let users = await User
+        .find({})
+        .lean();
       ctx.body = users;
       await next();
     })
