5c210d7c10b0566ad3425e5ccd07590fa110ec1e
oleglustenko <oleg.lustenko@gmail.com>
2016-08-16 11:03:34 +0300

    ok

--- src/server.js
+++ src/server.js
@@ -33,6 +33,7 @@ app.use(async(ctx,next)=> {
   console.log('ok');
   console.log('ok_22');
   console.log('ok_22');
+  console.log('ok_22');
   let location = createLocation(ctx.req.url);
   
   match({ routes, location }, (error, redirectLocation, renderProps) => {
